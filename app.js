c93eec22a045183c9074318746ca419cc41e1f53
Bailey Dalton <baileydalton007@gmail.com>
2019-12-23 20:27:50 -0500

    Adds style.css, and makes output an unordered list

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     sortArray = [];
+    var ul = document.getElementById("lastEntries");
+
     for (let i = 0; i < localStorage.length; i++) {
         sortArray.push([Number(localStorage.key(i).substring(5)), localStorage.getItem(localStorage.key(i))]);
         sortArray = sortArray.sort(function(a, b){return a[0]-b[0]});
     }
 
     for (let i = 0; i < sortArray.length; i++) {
-                document.getElementById("lastEntries").innerText += sortArray[i][1] + "\n";
+                li = document.createElement("li");
+                li.appendChild(document.createTextNode(sortArray[i][1] + "\n"));
+                li.setAttribute("id", "element" + i);
+                ul.appendChild(li);
+                //document.getElementById("lastEntries").innerText += sortArray[i][1] + "\n";
             }
         }
     
